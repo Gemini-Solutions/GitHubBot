bf86fa302d75e7034263212540c2b2d4ec6c3313
Chaitanya Ashok Raj <Chaitanya.Raj@Geminisolutions.com>
2024-04-22 13:21:40 +0530

    update

--- app.py
+++ app.py
@@ -1,48 +1,116 @@
-from langchain_community.embeddings import huggingface
-from langchain_community.document_loaders import TextLoader, DirectoryLoader
-from langchain_community.llms import huggingface_hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.documents import Document
+# from langchain_community.embeddings.huggingface import 
+from langchain_community.document_loaders.text import TextLoader
+from langchain_community.document_loaders.git import GitLoader
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.directory import DirectoryLoader
+from langchain_community.document_loaders.python import PythonLoader
+from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
+
+# from langchain_community.llms import huggingface_hub
+# from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+# from langchain_core.documents import Document
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
-from langchain_community.vectorstores import chroma, faiss
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders.parsers.language import LanguageParser
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.vectorstores.lancedb import LanceDB
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 import os
-import sys
-import re
-from urllib.parse import urlparse
-import ast
 from git import Repo
+import sys
 import streamlit as st
 from dotenv import load_dotenv
+from tempfile import TemporaryDirectory
 load_dotenv()
-def get_vectorstore_from_text(repo_name):
-     file = f"./{repo_name}_analysis.txt"
-     document = TextLoader(file).load()
-     # document = DirectoryLoader("./repo_text", glob=f"{repo_name}_analysis.txt", loader_cls=TextLoader).load()
-     # document = DirectoryLoader(f"./{repo_name}/cloned_repo", glob="**/*.py", loader_cls=PythonLoader).load()
-     os.remove(f"./{repo_name}_analysis.txt")
-     # loader = WebBaseLoader(url)
-     # document = loader.load()
-     text_splitter = RecursiveCharacterTextSplitter()
-     document_chunks = text_splitter.split_documents(document)
-     embeddings = OpenAIEmbeddings(api_key=os.getenv('OPENAI_API_KEY'))
-     # embeddings = huggingface.HuggingFaceEmbeddings()
-     st.session_state.vector_store = chroma.Chroma()
-     # for x in range(len(vector_store)):
-          # vector_store.delete(ids=[x])
-     # print(vector_store.delete_collection())
-     st.session_state.vector_store = chroma.Chroma.from_documents(document_chunks, embeddings)
-     # return st.session_state.vector_store
+HF_API_KEY = os.getenv('HF_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+def create_directory_loader(file_type, directory_path):
+     loaders = {
+          '.py': PythonLoader,
+          '.txt': TextLoader,
+          '.md': UnstructuredMarkdownLoader,
+     }
+     return DirectoryLoader(
+          path=directory_path,
+          glob=f"**/*{file_type}",
+          loader_cls=loaders[file_type],
+     )
+
+def get_vectorstore_from_text(repo_url):
+     temp = f"./{repo_url}"
+     with TemporaryDirectory() as temp:
+          # loader = GitLoader(
+          #      clone_url=repo_url,
+          #      repo_path=temp+"/test_repo",
+          #      branch='master',
+          #      file_filter=lambda file_path: file_path.endswith('.py')
+          # )
+
+          # document = loader.load()
+
+          Repo.clone_from(repo_url, to_path=temp+"/test_repo")
+          # loader = GenericLoader.from_filesystem(
+          #      temp+"/test_repo",
+          #      glob="**/*",
+          #      suffixes=[".py", ".txt"],
+          #      exclude=["**/non-utf8-encoding.py"],
+          #      parser=LanguageParser()
+          # )
+          # docs = loader.load()
+
+          py_loader = create_directory_loader('.py', temp+"/test_repo")
+          # txt_loader = create_directory_loader('.txt', temp+"/test_repo")
+          # md_loader = create_directory_loader('.md', temp+"/test_repo")
+
+          py_documents = py_loader.load()
+          # txt_documents = txt_loader.load()
+          # md_documents = md_loader.load()
+          docs = py_documents
+          # root_dir = temp+"/test_repo"
+          # docs = []
+          # for dirpath, dirnames, filenames in os.walk(root_dir):
+          #      for file in filenames:
+          #           file_extension = os.path.splitext(file)[1]
+          #           if file_extension in ['.py', '.txt']:
+          #                try:
+          #                     loader = TextLoader(os.path.join(dirpath, file), encoding='utf-8')
+          #                     docs.extend(loader.load_and_split())
+          #                except Exception as e:
+          #                     pass
+
+          print(len(docs))
+          text_splitter = RecursiveCharacterTextSplitter(
+                    # language=Language.PYTHON,
+                    chunk_size=1000, chunk_overlap=50
+               )
+          document_chunks = text_splitter.split_documents(docs)
+          print(document_chunks)
+          embeddings = OpenAIEmbeddings(
+               model='text-embedding-ada-002'
+          )
+          # embeddings = huggingface.HuggingFaceEmbeddings()
+          # st.session_state.vector_store = chroma.Chroma()
+          # for x in range(len(vector_store)):
+               # vector_store.delete(ids=[x])
+          # print(vector_store.delete_collection())
+          # if "vector_store" not in st.session_state:
+               # st.session_state.vector_store = Chroma.from_documents(document_chunks, embeddings)
+          vector_store = LanceDB.from_documents(document_chunks, embeddings)
+          return vector_store
 
 def get_context_retriever_chain(vector_store):
-     llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+     llm = ChatOpenAI(model='gpt-3.5-turbo')
      # llm = huggingface_hub.HuggingFaceHub(
-          # huggingfacehub_api_token=os.getenv('HF_API_KEY'),
+          # huggingfacehub_api_token=HF_API_KEY,
           # task='text-generation'
           # repo_id='CohereForAI/c4ai-command-r-plus'
      # )
@@ -50,8 +118,13 @@ def get_context_retriever_chain(vector_store):
      
      prompt = ChatPromptTemplate.from_messages([
           MessagesPlaceholder(variable_name="chat_history"),
-          ("user", "{input}"),
-          ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+          ("human", "{input}"),
+          ("system", '''
+                    Given the above conversation and the latest user question,
+                    which might reference context in the chat history,
+                    generate a standalone question which can be understood without the chat history.
+                    Do NOT answer the question, just reformulate it if needed and otherwise return it as is.
+               ''')
      ])
      
      retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
@@ -59,165 +132,86 @@ def get_context_retriever_chain(vector_store):
 
 def get_conversational_rag_chain(retriever_chain): 
     
-     llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+     llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.2)
      # llm = huggingface_hub.HuggingFaceHub(
-          # huggingfacehub_api_token=os.getenv('HF_API_KEY'),
+          # huggingfacehub_api_token=HF_API_KEY,
           # task='text-generation'
-          # repo_id='CohereForAI/c4ai-command-r-plus'
      # )
      prompt = ChatPromptTemplate.from_messages([
-          ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+          ("system", '''
+                    Instructions:
+                    1. Answer based on context given below.
+                    2. Focus on repo/code.
+                    3. Consider:
+                         a. Purpose/features - describe.
+                         b. Functions/code - provide details/samples.
+                         c. Setup/usage - give instructions.
+                    Context: {context}
+               '''),
           MessagesPlaceholder(variable_name="chat_history"),
-          ("user", "{input}")
+          ("human", "{input}")
      ])
      
      stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
      return create_retrieval_chain(retriever_chain, stuff_documents_chain)
 
-def get_response(user_input):
-     retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+def get_response(user_input, vector_store):
+
+     retriever_chain = get_context_retriever_chain(vector_store)
      conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
-     
      response = conversation_rag_chain.invoke({
           "chat_history": st.session_state.chat_history,
           "input": user_input
      })
+
+
+     # simple RAG without history
+     # llm = ChatOpenAI(model='gpt-3.5-turbo')
+     # # llm = huggingface_hub.HuggingFaceHub(
+     # #           huggingfacehub_api_token=HF_API_KEY,
+     # #           task='text-generation'
+     # #      )
+     # retriever = st.session_state.vector_store.as_retriever()
+     # prompt = hub.pull("rlm/rag-prompt")
+     # rag_chain = (
+     #      {'context': retriever, 'question': RunnablePassthrough()}
+     #      | prompt
+     #      | llm
+     #      | StrOutputParser()
+     # )
+     # response = rag_chain.invoke(user_input)
+     return response
+
      
-     return response['answer']
-
-
-
-def extract_readme(repo_folder):
-     readme_content = None
-     readme_path = os.path.join(repo_folder, "README.md")
-     if os.path.exists(readme_path):
-          try:
-               with open(readme_path, "r", encoding="utf-8") as f:
-                    readme_content = f.read()
-          except Exception as e:
-               print(f"Error reading README file: {e}")
-     return readme_content
-
-def extract_code_files(repo_folder):
-     code_files = []
-     for root, dirs, files in os.walk(repo_folder):
-          # Filter out the .git folder
-          if '.git' in dirs:
-               dirs.remove('.git')
-          for file in files:
-               if file.endswith((".txt", ".py", ".md")):
-                    file_path = os.path.join(root, file)
-                    try:
-                         with open(file_path, "r", encoding="utf-8") as f:
-                              code_files.append(f.read())
-                    except Exception as e:
-                         print(f"Error reading file {file_path}: {e}")
-     return code_files
-
-def preprocess_code(code):
-     cleaned_code = re.sub(r'#.*?\n', '\n', code)
-     cleaned_code = re.sub(r'""".*?"""', '', cleaned_code, flags=re.DOTALL)
-     return cleaned_code
-
-def analyze_code(code):
-     function_names = []
-     try:
-          parsed_ast = ast.parse(code)
-          for node in ast.walk(parsed_ast):
-               if isinstance(node, ast.FunctionDef):
-                    function_names.append(node.name)
-     except SyntaxError:
-          print("Syntax error occurred while parsing code.")
-     return len(function_names), function_names
-
-def analyze_code_files(code_files):
-     total_functions = 0
-     all_function_names = []
-
-     for code in code_files:
-          num_functions, function_names = analyze_code(code)
-          total_functions += num_functions
-          all_function_names.extend(function_names)
-
-     return total_functions, all_function_names
-# @st.cache
-def save_repo_analysis(repo_url, repo_name):
-     try:
-          # Clone the repository
-          repo_folder = "temp_repo"
-          Repo.clone_from(repo_url, repo_folder)
-
-          # Extract README content
-          readme_content = extract_readme(repo_folder)
-
-          # Extract code files
-          code_files = extract_code_files(repo_folder)
-          preprocessed_code_files = [preprocess_code(code) for code in code_files]
-
-          # Analyze code files
-          total_functions, all_function_names = analyze_code_files(preprocessed_code_files)
-
-          # Generate text for analysis
-          analysis_text = f"README CONTENT:\n{readme_content}\n\n"
-          analysis_text += f"Total functions: {total_functions}\n"
-          analysis_text += f"Function names: {', '.join(all_function_names)}\n"
-
-          # Write analysis text to file
-          output_file = f"{repo_name}_analysis.txt"
-          with open(output_file, "w", encoding="utf-8") as f:
-               f.write("")
-               f.write(analysis_text)
-
-          # Remove temporary repository folder
-          if os.name == "nt":  # Windows
-               os.system(f"rmdir /s /q {repo_folder}")
-          else:  # Unix-like (Linux, macOS)
-               os.system(f"rm -rf {repo_folder}")
-
-          print(f"Repository analysis saved to '{output_file}'")
-     except Exception as e:
-          print(f"Error saving repository analysis: {e}")
-def extract_repo_name(repo_url):
-     parsed_url = urlparse(repo_url)
-     repo_name = parsed_url.path.strip("/").split("/")[-1]
-     if repo_name.endswith(".git"):
-          repo_name = repo_name[:-4]  # Remove the .git suffix
-     repo_name = repo_name.replace("-", "_").replace(" ", "_")
-     return repo_name
 # app config
 def main():
-     # st.session_state.clear(
-     __import__('pysqlite3')
-     sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+     # __import__('pysqlite3')
+     # sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
      st.set_page_config(page_title="Chat", layout='wide')
      st.title("GitHub Bot")
      with st.sidebar:
-          st.header("Enter URL")
+          st.header("Enter GitHub Repository URL")
           git_url = st.text_input("")
 
      if git_url is None or git_url == "":
-          st.info("Please enter a website URL")
+          st.info("Chat is Empty...")
 
      else:
           if "chat_history" not in st.session_state:
                st.session_state.chat_history = [
                     AIMessage(content="Ask me about the repository..."),
                ]
-          if "vector_store" not in st.session_state:
-               repo_name = extract_repo_name(git_url)
-               save_repo_analysis(git_url, repo_name)
-               get_vectorstore_from_text(repo_name)
+          vector_store = get_vectorstore_from_text(git_url)
+          st.write(vector_store)
 
-          if 'git_url' not in st.session_state:
-               st.session_state.git_url = git_url
-               st.write(st.session_state)
-          
           # user input
           user_query = st.chat_input("Type your message here...")
           if user_query is not None and user_query != "":
-               response = get_response(user_query)
+               response = get_response(user_query, vector_store)
+               # st.write(st.session_state.chat_history)
                st.session_state.chat_history.append(HumanMessage(content=user_query))
-               st.session_state.chat_history.append(AIMessage(content=response))
+               st.session_state.chat_history.append(AIMessage(content=response['answer']))
+               # st.write(st.session_state)
           
 
           # conversation
@@ -232,3 +226,38 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
